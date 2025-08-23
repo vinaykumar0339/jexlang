67a075661303fdddf7371ca50d7580ccbb32c7de
vinaykumar0339 <vinay9961@gmail.com>
2025-08-24 03:03:25 +0530

    feat: remove unused JexEvaluator example code from index.ts

--- packages/jexlang-ts/src/index.ts
+++ packages/jexlang-ts/src/index.ts
@@ -1,10 +1,1 @@
 export * from "./eval/JexEvaluator";
-
-import { JexEvaluator } from "./eval/JexEvaluator";
-
-const jexEvaluator = new JexEvaluator();
-
-const result = jexEvaluator.evaluate(`
-    "Hello world" | capitalize
-`);
-console.log(result);
