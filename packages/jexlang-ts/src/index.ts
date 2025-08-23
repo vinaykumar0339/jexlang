901001c0aa24e581694157a6e59e993d08dddec9
vinaykumar0339 <vinay9961@gmail.com>
2025-08-24 02:36:31 +0530

    feat: remove unused JexEvaluator example code from index.ts

--- packages/jexlang-ts/src/index.ts
+++ packages/jexlang-ts/src/index.ts
@@ -1,28 +1,2 @@
-import { JexEvaluator } from "./eval/JexEvaluator";
-
-const jexEvaluator = new JexEvaluator({
-    x: 40,
-    user: {
-        age: 10
-    }
-});
-
-jexEvaluator.setFunction('max', (...args) => {
-    console.log(args);
-    return Math.max(...args as number[]);
-});
-
-const output = jexEvaluator.evaluate(`
-    con = {
-        "name": "vinay"
-    }
-    con.name = "vinay2"
-    con
-
-    ar = [1, 2, 3]
-    ar[1] = 20
-    ar
-`);
-
-console.log(output);
+export * from "./eval/JexEvaluator";
 
