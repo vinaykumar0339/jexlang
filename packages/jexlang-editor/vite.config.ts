d9a02da751b361a8e13a9e3add27e8f43c535b02
vinaykumar0339 <vinay9961@gmail.com>
2025-08-24 22:35:36 +0530

    chore: update TypeScript configuration and add Vite build setup
    - Changed TypeScript target from ES2020 to ES2022 and enabled class fields definition.
    - Updated module resolution to bundler and added various compiler options for improved linting and declaration file generation.
    - Introduced a new Vite configuration file to handle library builds with TypeScript declaration support and external dependencies.

---
+++ packages/jexlang-editor/vite.config.ts
@@ -0,0 +1,30 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+    plugins: [
+        dts({
+            tsconfigPath: resolve(__dirname, "tsconfig.json")
+        }),
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: "jexlang-ts",
+            formats: ["es", "umd"],
+            fileName: (format) => `jexlang-ts.${format}.js`
+        },
+        rollupOptions: {
+            external: ["monaco-editor"],
+            output: {
+                globals: {
+                    "monaco-editor": "monaco-editor"
+                }
+            }
+        },
+        sourcemap: true,
+        // Ensure declaration files are included
+        emptyOutDir: false,
+    },
+})
